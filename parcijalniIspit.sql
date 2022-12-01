39d1ee07b26c8a418eaa6591666bb42159cbf2ce
Matemateo10 <80220715+Matemateo10@users.noreply.github.com>
2022-12-01 15:56:53 +0100

    Add files via upload

---
+++ parcijalniIspit.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 01, 2022 at 03:50 PM
+-- Server version: 8.0.31-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `parcijalniIspit`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ProsjekPlacaZaposlenika` ()  BEGIN
+SELECT
+AVG(placa)`prosjek placa svih zaposlenika`
+FROM place;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `odjeli`
+--
+
+CREATE TABLE `odjeli` (
+  `odjelID` int UNSIGNED NOT NULL,
+  `imeOdjela` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `odjeli`
+--
+
+INSERT INTO `odjeli` (`odjelID`, `imeOdjela`) VALUES
+(1, 'računovodstvo'),
+(2, 'prodaja'),
+(3, 'marketing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `place`
+--
+
+CREATE TABLE `place` (
+  `zaposlenikID` int UNSIGNED NOT NULL,
+  `placa` int UNSIGNED NOT NULL,
+  `odDatuma` date NOT NULL,
+  `doDatuma` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `place`
+--
+
+INSERT INTO `place` (`zaposlenikID`, `placa`, `odDatuma`, `doDatuma`) VALUES
+(1, 8000, '2020-01-01', '2020-01-01'),
+(2, 5000, '2020-01-01', '2020-01-01'),
+(3, 8000, '2020-01-01', '2020-01-01'),
+(4, 5000, '2020-01-01', '2020-01-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `radnoMjesto`
+--
+
+CREATE TABLE `radnoMjesto` (
+  `zaposlenikID` int UNSIGNED NOT NULL,
+  `radnoMjesto` varchar(255) NOT NULL,
+  `odDatuma` date NOT NULL,
+  `doDatuma` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `radnoMjesto`
+--
+
+INSERT INTO `radnoMjesto` (`zaposlenikID`, `radnoMjesto`, `odDatuma`, `doDatuma`) VALUES
+(1, 'tajnik', '2020-01-01', '2021-01-01'),
+(2, 'tajnica', '2020-02-02', '2021-02-02'),
+(3, 'domar', '2020-03-03', '2021-03-03'),
+(4, 'direktor', '2020-01-01', '2021-03-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voditeljOdjela`
+--
+
+CREATE TABLE `voditeljOdjela` (
+  `zaposlenikID` int UNSIGNED NOT NULL,
+  `odjelID` int UNSIGNED NOT NULL,
+  `odDatuma` date NOT NULL,
+  `doDatuma` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `voditeljOdjela`
+--
+
+INSERT INTO `voditeljOdjela` (`zaposlenikID`, `odjelID`, `odDatuma`, `doDatuma`) VALUES
+(1, 3, '2000-01-01', '2022-09-09'),
+(2, 2, '2000-01-01', '2022-09-09'),
+(3, 1, '2000-01-01', '2022-09-09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zaposlenici`
+--
+
+CREATE TABLE `zaposlenici` (
+  `zaposlenikID` int UNSIGNED NOT NULL,
+  `ime` varchar(255) NOT NULL,
+  `prezime` varchar(255) NOT NULL,
+  `datumRodjenja` date NOT NULL,
+  `spol` varchar(255) NOT NULL,
+  `datumZaposlenja` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `zaposlenici`
+--
+
+INSERT INTO `zaposlenici` (`zaposlenikID`, `ime`, `prezime`, `datumRodjenja`, `spol`, `datumZaposlenja`) VALUES
+(1, 'ivan', 'ivic', '1999-01-01', 'muški', '2022-10-10'),
+(2, 'danijela', 'danic', '1990-02-02', 'žensko', '2022-05-05'),
+(3, 'roki', 'boki', '1989-03-03', 'muški', '2022-03-03'),
+(4, 'darko', 'daric', '1988-04-04', 'muški', '2022-04-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `zaposleniciOdjeli`
+--
+
+CREATE TABLE `zaposleniciOdjeli` (
+  `zaposlenikID` int UNSIGNED NOT NULL,
+  `odjelID` int UNSIGNED NOT NULL,
+  `odDatuma` date NOT NULL,
+  `doDatuma` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `zaposleniciOdjeli`
+--
+
+INSERT INTO `zaposleniciOdjeli` (`zaposlenikID`, `odjelID`, `odDatuma`, `doDatuma`) VALUES
+(1, 1, '2019-01-01', '2022-07-07'),
+(2, 2, '2019-01-01', '2022-07-07'),
+(3, 3, '2019-01-01', '2022-07-07');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `odjeli`
+--
+ALTER TABLE `odjeli`
+  ADD PRIMARY KEY (`odjelID`);
+
+--
+-- Indexes for table `place`
+--
+ALTER TABLE `place`
+  ADD PRIMARY KEY (`zaposlenikID`,`odDatuma`);
+
+--
+-- Indexes for table `radnoMjesto`
+--
+ALTER TABLE `radnoMjesto`
+  ADD PRIMARY KEY (`zaposlenikID`,`radnoMjesto`,`odDatuma`);
+
+--
+-- Indexes for table `voditeljOdjela`
+--
+ALTER TABLE `voditeljOdjela`
+  ADD PRIMARY KEY (`zaposlenikID`,`odjelID`),
+  ADD KEY `odjelID` (`odjelID`);
+
+--
+-- Indexes for table `zaposlenici`
+--
+ALTER TABLE `zaposlenici`
+  ADD PRIMARY KEY (`zaposlenikID`);
+
+--
+-- Indexes for table `zaposleniciOdjeli`
+--
+ALTER TABLE `zaposleniciOdjeli`
+  ADD PRIMARY KEY (`zaposlenikID`,`odjelID`),
+  ADD KEY `odjelID` (`odjelID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `place`
+--
+ALTER TABLE `place`
+  ADD CONSTRAINT `place_ibfk_1` FOREIGN KEY (`zaposlenikID`) REFERENCES `zaposlenici` (`zaposlenikID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `radnoMjesto`
+--
+ALTER TABLE `radnoMjesto`
+  ADD CONSTRAINT `radnoMjesto_ibfk_1` FOREIGN KEY (`zaposlenikID`) REFERENCES `zaposlenici` (`zaposlenikID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `voditeljOdjela`
+--
+ALTER TABLE `voditeljOdjela`
+  ADD CONSTRAINT `voditeljOdjela_ibfk_1` FOREIGN KEY (`zaposlenikID`) REFERENCES `zaposlenici` (`zaposlenikID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `voditeljOdjela_ibfk_2` FOREIGN KEY (`odjelID`) REFERENCES `odjeli` (`odjelID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `zaposleniciOdjeli`
+--
+ALTER TABLE `zaposleniciOdjeli`
+  ADD CONSTRAINT `zaposleniciOdjeli_ibfk_1` FOREIGN KEY (`zaposlenikID`) REFERENCES `zaposlenici` (`zaposlenikID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `zaposleniciOdjeli_ibfk_2` FOREIGN KEY (`odjelID`) REFERENCES `odjeli` (`odjelID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
